b4c6d8fc59186ecb7d6ddb2e3b42872daed98960
AlessandroLembo <alessandrolembo2@gmail.com>
2022-12-12 16:57:28 +0100

    calcolo del vincitore e stampa in pagina

--- script.js
+++ script.js
@@ -18,22 +18,41 @@ stampa un messaggio appropriato sull’esito del controllo.*/
 //* ESERCIZIO 1
 /* Analisi
 
-1 - Prendo l'elemento dalla pagina
-2 - Genrerare un numero random per il pc
+1 - Prendo gli elementi dalla pagina
+2 - Generare un numero random per il pc
 3 - Generare un numero random per il giocatore
 4 - Confrontare i numeri e stabilire chi è il vincitore
 5 - Stampiamo in pagina i numeri e il risultato 
 */
 
-// 1 - Prendere l'elemento dalla pagina
+// 1 - Prendo gli elementi dalla pagina
 const winnerUser = document.getElementById('winner');
-// console.log(winnerUser);
+const pcNumber = document.getElementById('pc-number');
+const userNumber = document.getElementById('user-number');
 
-// 2 - Generare un numero random per il pc
+
+// 2e3 - Generare i due numeri random
 const randomNumberPc = Math.floor(Math.random() * 6) + 1;
-console.log(`Il dado lanciato dal pc ha fatto ${randomNumberPc}`);
+console.log(`Il dado lanciato dal Pc ha fatto ${randomNumberPc}`);
 
 const randomNumberUser = Math.floor(Math.random() * 6) + 1;
 console.log(`Il dado lanciato dall'utente ha fatto ${randomNumberUser}`);
+
+// 4 - Stabilire chi è il vincitore
+if (randomNumberPc === randomNumberUser) {
+    console.log(`Pareggio!: rilancia i dadi`);
+    winnerUser.innerText = (`Pareggio!: Rilancia i dadi`);
+} else if (randomNumberPc > randomNumberUser) {
+    console.log(`Il vincitore è il pc`);
+    winnerUser.innerText = (`Il vincitore è il Pc con il numero ${randomNumberPc}`);
+} else {
+    console.log(`Il vincitore è l'utente`);
+    winnerUser.innerText = (`Il vincitore è l'utente con il numero ${randomNumberUser}`);
+}
+
+// 5 - Stampo in pagina i numeri e il risultato
+pcNumber.innerText = (`Il dado del Pc ha fatto ${randomNumberPc}`);
+userNumber.innerText = (`Il dado dell'utente ha fatto ${randomNumberUser}`);
+
 
 
